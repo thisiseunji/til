893fbc314b259caa0da9682d0e51b9f2f54434cd
kimeunji <78680486+thisiseunji@users.noreply.github.com>
2022-08-29 19:16:22 +0900

    sieve of eratosthenes

--- sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes.py
@@ -1,2 +1,12 @@
 def sieve_of_eratosthenes(n):
-    pass
+    prime_n = [2, 3, 5, 7]
+    count = 0
+    for i in range(1, n + 1):
+        for j in prime_n:
+            if i != j and i % j == 0:
+                count += 1
+                break
+              
+    return n-count-1
+
+print(sieve_of_eratosthenes(20))
